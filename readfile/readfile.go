1ea69239ed0bc3b20ec5afd6f27d508c4e230e6f
ombima56 <ombimahillary6@gmail.com>
2024-09-21 10:22:24 +0300

    Move readfile.go and readfile_test.go to readfile folder for better organization

---
+++ readfile/readfile.go
@@ -0,0 +1,27 @@
+package readfile
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+)
+
+func ReadData(filePath string) ([]float64, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var data []float64
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		val, err := strconv.ParseFloat(line, 64)
+		if err != nil {
+			return nil, err
+		}
+		data = append(data, val)
+	}
+	return data, scanner.Err()
+}
